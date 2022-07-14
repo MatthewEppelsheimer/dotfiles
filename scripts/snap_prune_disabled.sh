62c33a0324e51e4bc6bfe012c0fda0ec5d1163a2
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2022-07-14 13:39:27 -0700

    prune snaps as sudo

--- scripts/snap_prune_disabled.sh
+++ scripts/snap_prune_disabled.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # https://superuser.com/a/1330590
 # https://askubuntu.com/questions/895634/old-ubuntu-core-snap-versions-are-filling-up-disk-space
 
 snap list --all | awk '/disabled/{print $1, $3}' |
   while read snapname revision; do
-    snap remove "$snapname" --revision="$revision"
+    sudo snap remove "$snapname" --revision="$revision"
   done
 
